3158974490b7f8da2d902f6e9a0556d604ab4403
wick <123456@gmail.com>
2024-06-06 22:53:16 +0800

    day02第一个接口课程分类查询

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.xuecheng.content;
 
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XuechengPlusContentServiceApplicationTests {
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
 
     @Test
     void contextLoads() {
+        CourseBase courseBase = courseBaseMapper.selectById(74L);
+        Assertions.assertNotNull(courseBase);
+        System.out.println(courseBase);
+
     }
 
 }
